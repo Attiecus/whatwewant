9632947b1a8d996b7dd3d23211ed8a80ae630b11
Attiecus <125663900+Attiecus@users.noreply.github.com>
2024-05-28 18:12:42 +0400

    Update voting.py

--- voting.py
+++ voting.py
@@ -17,11 +17,11 @@ geocoder = OpenCageGeocode(OPENCAGE_API_KEY)
 NEWS_API_KEY = 'c18531a160cb4b729778ecbf3c643ead'  # Replace with your NewsAPI key
 IPINFO_API_KEY = 'f2439f60dfe99d'  # Replace with your ipinfo API key
 
-def fetch_news(api_key, query=None, category=None, country='us'):
+def fetch_news(api_key, query=None, category=None):
     if query:
         url = f'https://newsapi.org/v2/everything?q={query}&apiKey={api_key}'
     else:
-        url = f'https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={api_key}'
     response = requests.get(url)
     return response.json()
 
